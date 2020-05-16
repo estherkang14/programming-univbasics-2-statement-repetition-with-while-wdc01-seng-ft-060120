870f8c8838d46dc922ddcde45a931d459cf8a435
Esther Kang <estherkang14@gmail.com>
2020-05-16 16:56:06 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,5 +1,9 @@
 # Write your code here
-count = 0 
-while count < 3 do
-  puts "I am the #{count}, I love to count!"
-end 
+count = 10 
+happy_new_year = 0 
+while count >= 1 do
+  puts count
+  count -= 1 
+end
+puts "Happy New Year!"
+  
